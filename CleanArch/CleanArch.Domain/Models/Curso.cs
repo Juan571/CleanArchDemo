acd4bac0be543dd353bacc1abd4017b5932f20df
Usuario <Usuario@DESKTOP-7IIN9B1>
2020-04-06 18:20:43 -0400

    creando el modelo de curso en la capa de dominio

---
+++ CleanArch/CleanArch.Domain/Models/Curso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Models
+{
+    class Curso
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String ImageUrl { get; set; }
+
+
+    }
+}
